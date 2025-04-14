7554c0d4ddab8fb0b8236ad057eacc0a5719a2f2
Nevaeh0701 <162300874+Nevaeh0701@users.noreply.github.com>
2025-04-14 23:34:45 +0900

    webconfig 변경

--- src/main/java/com/travel/security/auth/api/AuthController.java
+++ src/main/java/com/travel/security/auth/api/AuthController.java
@@ -1,16 +1,15 @@
 package com.travel.security.auth.api;
 
-import com.travel.security.auth.dto.request.RegisterRequest;
-import com.travel.security.auth.dto.response.AuthResponse;
-import com.travel.security.auth.service.AuthService;
-import io.swagger.v3.oas.annotations.Operation;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import com.travel.security.auth.dto.request.RegisterRequest;
+import com.travel.security.auth.dto.response.AuthResponse;
+import com.travel.security.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -19,19 +18,15 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthService authService;
 
-    @Operation(
-            summary = "카카오 로그인")
+    @Operation(summary = "카카오 로그인")
     @PostMapping("/register")
-    public ResponseEntity<AuthResponse> loginByKakao(
-            @RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity<AuthResponse> loginByKakao(@RequestBody RegisterRequest registerRequest) {
         return new ResponseEntity<>(authService.signIn(registerRequest), HttpStatus.OK);
     }
 
-    @Operation(
-            summary = "회원 탈퇴")
+    @Operation(summary = "회원 탈퇴")
     @DeleteMapping("/withdraw")
     public void withdraw(@AuthenticationPrincipal Long memberId) {
         authService.withdraw(memberId);
     }
-
 }
