f7ab14096b6f5755881644b5c9dd184e160fac33
Nevaeh0701 <162300874+Nevaeh0701@users.noreply.github.com>
2025-04-28 22:06:04 +0900

    llm 기반 태그 기능 구현

--- src/main/java/com/travel/domain/place/service/PlaceService.java
+++ src/main/java/com/travel/domain/place/service/PlaceService.java
@@ -1,13 +1,18 @@
 package com.travel.domain.place.service;
 
-import com.travel.domain.place.dto.TourAttractionDetailDto;
-import com.travel.domain.place.dto.request.GoogleRequest;
-import com.travel.domain.place.dto.TourAttractionListDto;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.travel.domain.llm.dto.TourAttractionReviewDto;
+import com.travel.domain.place.dto.TourAttractionLLMDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.travel.domain.place.dto.TourAttractionDetailDto;
+import com.travel.domain.place.dto.TourAttractionListDto;
+import com.travel.domain.place.dto.request.GoogleRequest;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -15,16 +20,42 @@ public class PlaceService {
     private final GoogleService googleService;
     private final LLMService llmService;
 
+    /*
+    정적 키워드로 장소 리스트 반환 함수
+     */
     @Transactional
-    public TourAttractionListDto searchPlace(GoogleRequest googleRequest){
-            TourAttractionListDto tourAttractionListDto = googleService.searchTourAttraction(googleRequest);
-            llmService.generateTagsWithLLM(tourAttractionListDto);
-            return tourAttractionListDto;
+    public TourAttractionListDto searchPlace(GoogleRequest googleRequest) {
+        TourAttractionListDto tourAttractionListDto =
+                googleService.searchTourAttraction(googleRequest);
+
+        return tourAttractionListDto;
     }
 
-
+    /*
+   정적 키워드로 장소 '세부정보' 리스트 반환 함수
+    */
     public List<TourAttractionDetailDto> searchPlaceDetail(GoogleRequest googleRequest) {
-        TourAttractionListDto tourAttractionListDto = googleService.searchTourAttraction(googleRequest);
+        TourAttractionListDto tourAttractionListDto =
+                googleService.searchTourAttraction(googleRequest);
         return googleService.getDetailedTourAttractions(tourAttractionListDto);
     }
+
+
+
+    /*
+    정적 키워드로 장소 세부 정보 및 llm 태깅 정보 포함하는 함수
+     */
+    public List<TourAttractionLLMDto> saveTourAttraction(GoogleRequest googleRequest) {
+        TourAttractionListDto tourAttractionListDto =
+                googleService.searchTourAttraction(googleRequest);
+
+       List<TourAttractionDetailDto> tourAttractionDetailDtos = googleService.getDetailedTourAttractions(tourAttractionListDto);
+
+
+
+       return llmService.generateTagsWithGemini(tourAttractionDetailDtos);
+    }
+
+
+
 }
