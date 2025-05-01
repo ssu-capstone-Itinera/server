ba0ae7dc12d08e11675a49d21a39ebae47b5170e
Nevaeh0701 <162300874+Nevaeh0701@users.noreply.github.com>
2025-05-01 18:07:14 +0900

    tripresponse 생성

--- src/main/java/com/travel/domain/trip/service/TripService.java
+++ src/main/java/com/travel/domain/trip/service/TripService.java
@@ -2,18 +2,23 @@ package com.travel.domain.trip.service;
 
 import com.travel.domain.member.entity.Member;
 import com.travel.domain.trip.dao.TripRepository;
+import com.travel.domain.trip.dto.response.TripResponse;
 import com.travel.domain.trip.entity.Trip;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class TripService {
     private final TripRepository tripRepository;
 
-    public List<Trip> getTripList(Member member) {
-        return tripRepository.findByMember(member);
+    public List<TripResponse> getTripList(Member member) {
+        List<Trip> tripList = tripRepository.findByMember(member);
+        return tripList.stream()
+                .map(TripResponse::of)
+                .collect(Collectors.toList());
     }
 }
