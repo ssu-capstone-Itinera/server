b254774d742fcb1f58a8c51fdff0528c0c130ef4
sehoon <csh000924@gmail.com>
2025-05-13 11:58:36 +0900

    DTO수정: GoogleRequest
    GoogleRequestDTO를 수정했습니다.
    1. lat, lng, radius 필드를 추가하고 사용자가 직접 입력하진 못하게 하였습니다
    2. keyword가 여러개 들어올 상황을 고려하여(ex: MOUNTAIN, BEACH) keywords를 사용자에게 받고
    이후 내부 구현으로 keyword를 하나씩 설정하면서 keyword로 api request가 전송됩니다
    3. pricelevel, myPlaceQuery, myPlaceAddress가 추가되었습니다.

--- src/main/java/com/travel/domain/datapipeline/google/dto/request/GoogleRequest.java
+++ src/main/java/com/travel/domain/datapipeline/google/dto/request/GoogleRequest.java
@@ -1,16 +1,51 @@
 package com.travel.domain.datapipeline.google.dto.request;
 
-import com.travel.domain.placetype.entity.tourattraction.ApiTag;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.travel.domain.placetype.entity.ApiTag;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Setter
 @Getter
 @AllArgsConstructor
 public class GoogleRequest {
+
+    @Schema(hidden = true)
+    @JsonIgnore
     private double lat;
+
+    @Schema(hidden = true)
+    @JsonIgnore
     private double lng;
+
+    @Schema(hidden = true)
+    @JsonIgnore
+    private Integer radius;
+
+    @Schema(description = "main keyword(domain.placetype.entity.ApiTag)", nullable = true)
+    private List<ApiTag> keywords;
+
+    @Schema(hidden = true)
+    @JsonIgnore
     private ApiTag keyword;
+
+    @Schema(description = "tourist_attraction / restaurant / cafe", nullable = true)
     private String placeType;
+
+    @Schema(description = "priceLevel(restaurant only or null)", nullable = true)
+    private Integer priceLevel;
+
+    @Schema(description = "myPlace keyword", nullable = true)
+    private String myPlaceQuery;
+
+    @Schema(description = "myPlace address", nullable = true)
+    private String myPlaceAddress;
+
+    @Schema(description = "maxResult")
     private int maxResults;
 }
