5c69b5d58547ea46ac0b3eac60b1cf82103fa9d5
Nevaeh0701 <162300874+Nevaeh0701@users.noreply.github.com>
2025-05-13 16:15:53 +0900

    placetype response 수정

--- src/main/java/com/travel/domain/placetype/dto/response/RestaurantResponse.java
+++ src/main/java/com/travel/domain/placetype/dto/response/RestaurantResponse.java
@@ -3,7 +3,11 @@ package com.travel.domain.placetype.dto.response;
 
 import com.travel.domain.place.dto.PlaceResponse;
 import com.travel.domain.placetype.entity.restaurant.RestaurantType;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class RestaurantResponse {
     private Long placeId;
     private String googleId;
