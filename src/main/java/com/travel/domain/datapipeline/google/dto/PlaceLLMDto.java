1efba872fa1fb9dfd64b203b8454d05f592f3c48
sehoon <csh000924@gmail.com>
2025-05-13 11:49:25 +0900

    DTO수정: TourAttractionLLMDto
    단순 이름수정입니다.

--- src/main/java/com/travel/domain/datapipeline/google/dto/PlaceLLMDto.java
+++ src/main/java/com/travel/domain/datapipeline/google/dto/PlaceLLMDto.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Getter
 @Builder
-public class TourAttractionLLMDto {
+public class PlaceLLMDto {
     private List<PlaceDetailDto> placeDetailDtoList;
     private List<SubjectiveTag> subjectiveTags;
 }
