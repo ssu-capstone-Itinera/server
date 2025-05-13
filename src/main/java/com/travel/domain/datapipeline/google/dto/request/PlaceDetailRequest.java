63868583cadb54fda2851b7a2524d6daaa5d8d6f
sehoon <csh000924@gmail.com>
2025-05-13 11:56:06 +0900

    DTO추가: PlaceDetailRequest
    searchDetail request시, google request객체 전체가 아니라 placeIds만을 담고있는 simple한 request를 보냅니다

---
+++ src/main/java/com/travel/domain/datapipeline/google/dto/request/PlaceDetailRequest.java
@@ -0,0 +1,12 @@
+package com.travel.domain.datapipeline.google.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PlaceDetailRequest {
+    private List<String> placeIds;
+}
