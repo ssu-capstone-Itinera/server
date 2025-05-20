874468c22e8512d3d1dc5e0e22b43c4273042867
Nevaeh0701 <162300874+Nevaeh0701@users.noreply.github.com>
2025-05-20 11:07:22 +0900

    UserPostLikeResponse 추가

---
+++ src/main/java/com/travel/domain/post/post/dto/response/UserPostLikeResponse.java
@@ -0,0 +1,21 @@
+package com.travel.domain.post.post.dto.response;
+
+import com.travel.domain.member.entity.Member;
+import com.travel.domain.post.post.domain.PostLike;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserPostLikeResponse {
+    private Long postId;
+
+
+    public static UserPostLikeResponse of(PostLike postLike) {
+        return UserPostLikeResponse.builder()
+                .postId(postLike.getPost().getId())
+                .build();
+    }
+}
