35627985bfcbf21fcbf82ab328a2c047b379c561
sehoon <csh000924@gmail.com>
2025-05-22 12:49:13 +0900

    ✨ feat: MyPlaceSelectRequest
    MyPlaceSelectRequest 클래스입니다

---
+++ src/main/java/com/travel/domain/place/dto/MyPlaceSelectRequest.java
@@ -0,0 +1,18 @@
+package com.travel.domain.place.dto;
+
+import com.travel.domain.place.entity.Place;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MyPlaceSelectRequest {
+
+    @Schema(description = "사용자가 작성한 myPlace의 이름입니다")
+    private String customName;
+
+    @Schema(description = "사용자가 선택한 장소입니다")
+    private Place myPlace;
+}
