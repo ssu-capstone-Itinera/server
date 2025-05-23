692ab549523e029b16c109324a8df17b4ad917dd
Nevaeh0701 <162300874+Nevaeh0701@users.noreply.github.com>
2025-05-23 20:55:45 +0900

    cors 포트 추가

--- src/main/java/com/travel/global/config/WebConfig.java
+++ src/main/java/com/travel/global/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
                 registry.addMapping("/**")
                         .allowedOriginPatterns(
                                 "http://localhost:8080",
+                                "http://localhost:5173",
                                 "http://3.36.60.210",
                                 "http://3.36.60.210:8080",
                                 "http://3.36.60.210:80")
