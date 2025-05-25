550f142c9162d302e090ec1115a40f86b0b67977
sehoon <csh000924@gmail.com>
2025-05-26 05:11:18 +0900

    신규 DTO: SimplePlaceDto
    프론트 측의 데이터값을 보고 만든 SimplePlace입니다.
    Place의 일부값만을 사용하고있고, category가 MY_PLACE일경우 MY_PLACE로 변경되어 저장되는 과정을 거치고, 일반 PLACE일경우 placeId로 검색하여 실제 place가 Itenerary에 저장됩니다.

---
+++ src/main/java/com/travel/domain/itinerary/dto/SimplePlaceDto.java
@@ -0,0 +1,18 @@
+package com.travel.domain.itinerary.dto;
+
+import com.travel.domain.categories.entity.Category;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class SimplePlaceDto {
+    private String address;
+    private Category category;
+    private Double lat;
+    private Double lng;
+    private String name;
+    private String placeGoogleId;
+    private Integer placeId;
+    private Double rating;
+}
